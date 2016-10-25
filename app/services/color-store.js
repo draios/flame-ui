a0c597e3214b76733e6cf2ead8851842f8a64a37
davideschiera <davide.schiera@gmail.com>
2016-10-24 20:01:49 -0700

    New color scheme.

--- app/services/color-store.js
+++ app/services/color-store.js
@@ -2,12 +2,23 @@
 
 import Ember from 'ember';
 
+const colors = [
+    '#66c2a5',
+    '#fc8d62',
+    '#8da0cb',
+    '#e78ac3',
+    // '#a6d854',
+    '#ffd92f',
+    '#e5c494',
+    '#b3b3b3'
+];
+
 export default Ember.Service.extend({
     init() {
         this._super.apply(this, arguments);
 
         this.setProperties({
-            colors:             d3.scale.category10(),
+            colors:             d3.scale.ordinal().range(colors),
             lastColorIndex:     0,
             containerNames:     {},
             containerNameList:  [],
